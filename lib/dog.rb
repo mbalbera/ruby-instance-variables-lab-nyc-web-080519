96d84bc9a177bb99423be990c9f2cc2579647029
Mason Balbera <mhbalbera@gmail.com>
2019-08-06 14:44:34 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+
+class Dog
+    def initialize 
+    end
+
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+
+   
+end 
+
+fido = Dog.new()
+snoopy = Dog.new()
+lassie = Dog.new()
